1a047bbf9c0b0d5d5bf5c05cc369f0a65dfbfbb8
LOCKEDFILE <41106126+LOCKEDFILE@users.noreply.github.com>
2024-09-02 18:32:12 +0900

    test2

--- ios/Classes/SendbirdNotificationHandlerPlugin.swift
+++ ios/Classes/SendbirdNotificationHandlerPlugin.swift
@@ -1,9 +1,8 @@
 import Flutter
 import UIKit
 
-public class SendbirdNotificationHandlerPlugin: NSObject, FlutterPlugin, FlutterStreamHandler, UNUserNotificationCenterDelegate {
+public class SendbirdNotificationHandlerPlugin: NSObject, FlutterPlugin, FlutterStreamHandler {
     
-    var _delegate : UNUserNotificationCenterDelegate?
     var _eventChannel : FlutterEventSink? = nil
     var _initialMessage: BirdMessage?
     
@@ -21,13 +20,6 @@ public class SendbirdNotificationHandlerPlugin: NSObject, FlutterPlugin, Flutter
 
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
-    
-    init(_delegate: UNUserNotificationCenterDelegate? = nil) {
-        self._delegate = nil
-        super.init()
-        // UNUserNotificationCenter.current().delegate = self
-        NotificationCenter.default.addObserver(self, selector: #selector(didFinish(_:)), name: Notification.Name(UIApplication.didFinishLaunchingNotification.rawValue), object: nil)
-    }
     
     @objc func didFinish(_ notification: NSNotification) {
         print("APP FINISHED LAUNCH")
@@ -67,43 +59,8 @@ public class SendbirdNotificationHandlerPlugin: NSObject, FlutterPlugin, Flutter
         return nil
     }
     
-    public  func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        guard let content = notification.request.content.userInfo["sendbird"] as? [String: Any] else {
-            return
-        }
-        print(content)
-        
-        let birdMessage = buildBirdMessage(content: content)
-        sendNotificationEvent(bird: birdMessage, event:"inactive")
-        return completionHandler([])
-        
-    }
     
-    public  func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
-        if (response.actionIdentifier == UNNotificationDefaultActionIdentifier) {
-            guard let content = response.notification.request.content.userInfo["sendbird"] as? [String: Any] else {
-                return
-            }
-            let birdMessage = buildBirdMessage(content: content)
-            sendNotificationEvent(bird: birdMessage, event: "active")
-            completionHandler()
-        }
-    }
-
     
-    func sendNotificationEvent(bird: BirdMessage, event: String) {
-        let jsonEncoder = JSONEncoder()
-        do {
-            let jsonData = try jsonEncoder.encode([event:bird])
-            let jsonString = String(data: jsonData, encoding: .utf8)
-            print(jsonString)
-            if(_eventChannel != nil) {
-                _eventChannel!(jsonString)
-            }
-        } catch {
-            print(error)
-        }
-    }
     
     func handleInitialNotification() -> String? {
         
